7da5dc9e85803432cb93691ca0124a8f965df520
BitWizardX <BitWizardX@pm.me>
2025-10-30 14:39:46 +0800

    ✨ feat(control-node): add ssm plugin support

--- iac/01-infra/scripts/control_node/init.sh
+++ iac/01-infra/scripts/control_node/init.sh
@@ -12,6 +12,9 @@ DOH_URL="https://cloudflare-dns.com/dns-query"
 AWS_SSM_AGENT_PACKAGE="amazon-ssm-agent.deb"
 AWS_SSM_AGENT_PACKAGE_URL="https://s3.ap-northeast-1.amazonaws.com/amazon-ssm-ap-northeast-1/latest/debian_amd64/amazon-ssm-agent.deb"
 
+AWS_SSM_PLUGIN_PACKAGE="session-manager-plugin.deb"
+AWS_SSM_PLUGIN_PACKAGE_URL="https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb"
+
 AWS_CLI_PACKAGE="awscliv2.zip"
 AWS_CLI_PACKAGE_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 
@@ -34,11 +37,16 @@ download_files() {
     --tcp-fastopen \
     --tcp-nodelay \
     -o "${AWS_SSM_AGENT_PACKAGE}" "${AWS_SSM_AGENT_PACKAGE_URL}" \
+    -o "${AWS_SSM_PLUGIN_PACKAGE}" "${AWS_SSM_PLUGIN_PACKAGE_URL}" \
     -o "${AWS_CLI_PACKAGE}" "${AWS_CLI_PACKAGE_URL}"
 }
 
 install_aws_ssm_agent() {
   apt-get install --yes "${WORKDIR}/${AWS_SSM_AGENT_PACKAGE}"
+}
+
+install_aws_ssm_plugin() {
+  apt-get install --yes "${WORKDIR}/${AWS_SSM_PLUGIN_PACKAGE}"
 }
 
 install_awscli() {
@@ -64,6 +72,7 @@ main() {
 
   download_files
   install_aws_ssm_agent
+  install_aws_ssm_plugin
   install_awscli
   install_ansible
 }
